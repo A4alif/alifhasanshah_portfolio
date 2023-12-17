5e478e1d68879585c34ab973dee24ec9cd79a535
alifhasanshah <alifhasanshah@gmail.com>
2023-12-17 13:57:19 +0600

    add resume link

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -33,7 +33,7 @@ const Banner = () => {
                 <Link
                   target="_blank"
                   to={
-                    "https://drive.google.com/file/d/1ntAz7cIyNgsC5dsv9pWRJfM6YFnP7_v1/view"
+                    "https://drive.google.com/file/d/10nUEUBbAF8VuFxHQibA8MyIiyE01kq1-/view?usp=sharing"
                   }
                 >
                   <button className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center me-2 mb-2">
